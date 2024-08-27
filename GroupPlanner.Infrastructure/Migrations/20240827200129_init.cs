d3d4b83c95ed9d59597b3faa9fc93203c931db35
G0409M <gabrysia.maslanka@gmail.com>
2024-08-27 22:04:42 +0200

    migration

---
+++ GroupPlanner.Infrastructure/Migrations/20240827200129_init.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupPlanner.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Details_Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Details_CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Details_Deadline = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EncodedName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
