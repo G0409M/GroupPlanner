ec7d0427822903b468312341b71bb052dba97843
G0409M <gabrysia.maslanka@gmail.com>
2024-09-11 10:22:41 +0200

    Task mistakes

--- GroupPlanner.MVC/wwwroot/js/Task/Edit.js
+++ GroupPlanner.MVC/wwwroot/js/Task/Edit.js
@@ -1,37 +1,43 @@
 ﻿$(document).ready(function () {
+
     const RenderSubtasks = (subtasks, container) => {
         container.empty();
+
         for (const subtask of subtasks) {
-            container.append(`
-            <div class ="card border-secondary mb-3" style = "max-width: 18 rem;">
-            <div class=" card-header">${subtask.deadline} </div>
-            <div class="card-body">
-            <h5 class="card-title"> ${subtask.description}</h5>
-            </div>
-            </div>
-            `)
+            container.append(
+                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">${subtask.deadline}</div>
+          <div class="card-body">
+            <h5 class="card-title">${subtask.description}</h5> 
+          </div>
+        </div>`)
         }
     }
 
+
     const LoadSubtasks = () => {
         const container = $("#subtasks")
-        const taskEncodedName = container.data("encodedName
-         $.ajax({
-             url: `/Task/${taskEncodedName}/Subtask`,
-             type: 'get',
-             success: function (data) {
-                 if (!data.length) {
-                     container.html( There are no subtask for this task.)
-                 } else{
-                     RenderSubtasks(data, container)
-                 }
-             },
-             error: function () {
-                 toatr["error"]("Something went wrong.")
-             }
-         })
+        const taskEncodedName = container.data("encodedName");
+
+        $.ajax({
+            url: `/Task/${taskEncodedName}/Subtask`,
+            type: 'get',
+            success: function (data) {
+                if (!data.length) {
+                    container.html("There are no subtask for this task.")
+                } else {
+                    RenderSubtasks(data, container)
+                }
+            },
+            error: function () {
+                toastr["error"]("Something went wrong")
+            }
+        })
     }
+
     LoadSubtasks()
+
+
     $("#createSubtaskModal form").submit(function (event) {
         event.preventDefault();
 
@@ -43,7 +49,7 @@
                 toastr["success"]("Created subtask.")
             },
             error: function () {
-                toatr["error"]("Something went wrong.")
+                toastr["error"]("Something went wrong.")
             }
         })
     });
