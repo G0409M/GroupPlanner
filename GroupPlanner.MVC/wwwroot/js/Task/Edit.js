857e561876d5d9467b7844c84c9df0339a066401
G0409M <gabrysia.maslanka@gmail.com>
2024-09-16 08:20:39 +0200

    detail/edit change

--- GroupPlanner.MVC/wwwroot/js/Task/Edit.js
+++ GroupPlanner.MVC/wwwroot/js/Task/Edit.js
@@ -1,40 +1,6 @@
 ﻿$(document).ready(function () {
 
-    const RenderSubtasks = (subtasks, container) => {
-        container.empty();
-
-        for (const subtask of subtasks) {
-            container.append(
-                `<div class="card border-secondary mb-3" style="max-width: 18rem;">
-          <div class="card-header">${subtask.deadline}</div>
-          <div class="card-body">
-            <h5 class="card-title">${subtask.description}</h5> 
-          </div>
-        </div>`)
-        }
-    }
-
-
-    const LoadSubtasks = () => {
-        const container = $("#subtasks")
-        const taskEncodedName = container.data("encodedName");
-
-        $.ajax({
-            url: `/Task/${taskEncodedName}/Subtask`,
-            type: 'get',
-            success: function (data) {
-                if (!data.length) {
-                    container.html("There are no subtask for this task.")
-                } else {
-                    RenderSubtasks(data, container)
-                }
-            },
-            error: function () {
-                toastr["error"]("Something went wrong")
-            }
-        })
-    }
-
+    
     LoadSubtasks()
 
 
@@ -47,6 +13,7 @@
             data: $(this).serialize(),
             success: function (data) {
                 toastr["success"]("Created subtask.")
+                LoadSubtasks()
             },
             error: function () {
                 toastr["error"]("Something went wrong.")
--- GroupPlanner.MVC/wwwroot/js/site.js
+++ GroupPlanner.MVC/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const RenderSubtasks = (subtasks, container) => {
+    container.empty();
 
-// Write your JavaScript code.
+    for (const subtask of subtasks) {
+        container.append(
+            `<div class="card border-secondary mb-3" style="max-width: 18rem;">
+          <div class="card-header">${subtask.deadline}</div>
+          <div class="card-body">
+            <h5 class="card-title">${subtask.description}</h5> 
+          </div>
+        </div>`)
+    }
+}
+
+
+const LoadSubtasks = () => {
+    const container = $("#subtasks")
+    const taskEncodedName = container.data("encodedName");
+
+    $.ajax({
+        url: `/Task/${taskEncodedName}/Subtask`,
+        type: 'get',
+        success: function (data) {
+            if (!data.length) {
+                container.html("There are no subtask for this task.")
+            } else {
+                RenderSubtasks(data, container)
+            }
+        },
+        error: function () {
+            toastr["error"]("Something went wrong")
+        }
+    })
+}
